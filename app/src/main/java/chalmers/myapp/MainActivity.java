76ca92f217be762c87771e1f94099ef806496533
Teklie <teklie@student.chalmers.se>
2019-01-04 22:35:51 +0100

    UI and back-end connecting code is added

--- app/src/main/java/chalmers/myapp/MainActivity.java
+++ app/src/main/java/chalmers/myapp/MainActivity.java
@@ -2,16 +2,27 @@ package chalmers.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = findViewById(R.id.edit_text);
+        textView= findViewById(R.id.text_view);
+
+
 
     }
+public void displayResults(View view){
 
+}
 }
